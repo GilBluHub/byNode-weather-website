041fb7bb16338d3d7419ac278d294276fe3497f4
GilBluHub <blupub@gmail.com>
2019-04-22 20:30:11 +0300

    Change the web Title

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
     res.render('index',{
-        title:'wether app',
+        title:'Weather app',
         name:'gil'
     })
 })
